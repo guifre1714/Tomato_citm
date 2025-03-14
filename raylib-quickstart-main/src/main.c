d3a623e936bcd233d65c9f842f8199b9b75d9cd8
mecMocmec <e.ibanez3426@gmail.com>
2025-03-14 09:45:35 +0100

    he fet molta feina pallassos
    ara apareix el fons
    apareix tmb el bomberman i es pot moure
    no hi ha col.lisions de res pero algo es algo
    mec

--- raylib-quickstart-main/src/main.c
+++ raylib-quickstart-main/src/main.c
@@ -12,36 +12,56 @@ by Jeffery Myers is marked with CC0 1.0. To view a copy of this license, visit h
 #include "resource_dir.h" // utility header for SearchAndSetResourceDir
 int main()
 {
-	Rectangle GetShapesTextureRectangle(void);
-	Rectangle drawArea = { 100, 100 };
-	Vector2 playerPos = { 400, 400 };
-	int spriteBomber1[2] = {0, 16};
+	//BASURERO DE CODI
+	// 
+	// draw some text using the default font
+	//DrawText("Jajaja aixo es una pestanya normal", 200, 200, 20, WHITE);
+
+
+
+
+
+
+	//CODI OFICIAL FUNCIONAL
+	const int screenWidth = 1280;
+	const int screenHeight = 800;
+
 	// Tell the window to use vsync and work on high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
 	// Create the window and OpenGL context
-	InitWindow(1280, 800, "El marc es un mentider");
+	InitWindow(screenWidth, screenHeight, "Bomberman Test");
 
 	// Utility function from resource_dir.h to find the resources folder and set it as the current working directory so we can load from it
 	SearchAndSetResourceDir("resources");
 
 	// Load a texture from the resources directory
-	Texture SpriteSheet = LoadTexture("General Sprites.png");
+	Texture Fons = LoadTexture("Sprites/Fons.png");//cal afegir el nom de cada carpeta on esta la imatge
+	Texture BMan = LoadTexture("Sprites/bomberman.png");
+
+	Vector2 BManPos = { (float)screenWidth / 2, (float)screenHeight / 2 };
+
+	SetTargetFPS(60);
 
 	// game loop
 	while (!WindowShouldClose()) // run the loop untill the user presses ESCAPE or presses the Close button on the window
 	{
+
+		if (IsKeyDown(KEY_RIGHT)) BManPos.x += 2.0f;
+		if (IsKeyDown(KEY_LEFT)) BManPos.x -= 2.0f;
+		if (IsKeyDown(KEY_UP)) BManPos.y -= 2.0f;
+		if (IsKeyDown(KEY_DOWN)) BManPos.y += 2.0f;
+
 		// drawing
 		BeginDrawing();
 
 		// Setup the back buffer for drawing (clear color and depth buffers)
-		ClearBackground(BLACK);
-
-		// draw some text using the default font
-		DrawText("Jajaja aixo es una pestanya normal", 200, 200, 20, WHITE);
+		ClearBackground(RAYWHITE);
 
 		// draw our texture to the screen
-		DrawTextureRec(SpriteSheet, drawArea, playerPos, WHITE);
+		DrawTexture(Fons, screenWidth / 2 - Fons.width / 2, screenHeight / 2 - Fons.height / 2, WHITE);
+		//DrawTexture(BMan, screenWidth / 2 - Fons.width / 2, screenHeight / 2 - Fons.height / 2, WHITE);
+		DrawTextureV(BMan, BManPos, WHITE);
 
 		// end the frame and get ready for the next one (display frame, poll input, etc...)
 		EndDrawing();
@@ -49,7 +69,7 @@ int main()
 
 	// cleanup
 	// unload our texture so it can be cleaned up
-	UnloadTexture(SpriteSheet);
+	UnloadTexture(Fons);
 
 	// destroy the window and cleanup the OpenGL context
 	CloseWindow();

c0528aa0ef1ff59772179845afabb6861f7a976e
guifre1714 <marcelpm2018@gmail.com>
2025-05-12 08:24:46 +0200

    interficie superior

--- raylib-quickstart-main/src/SourceMain.cpp
+++ raylib-quickstart-main/src/SourceMain.cpp
@@ -13,8 +13,8 @@ int main()
 {   
 
 	//basics per a funcionar
-	const int screenWidth = 650;
-	const int screenHeight = 800;
+	const int screenWidth = 1280;
+	const int screenHeight = 720;
 
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
 
@@ -34,6 +34,7 @@ int main()
 	Game game;
 
 	int puntuacio = 0;
+	int vida = 3;
 
 	Camera2D camera = { 0 };
 	camera.target.x = game.bomberman.bmanPos.x + 20.0f;
@@ -45,8 +46,12 @@ int main()
 
 	while (!WindowShouldClose())
 	{
-		string puntuacioText = "Score: " + to_string(puntuacio);
-		int textWidth = MeasureText(puntuacioText.c_str(), 20);
+		//interficie superior
+		string puntuacioText = to_string(puntuacio);
+		int textw1 = MeasureText(puntuacioText.c_str(), 500);
+
+		string vidaText = "Left " + to_string(vida-1);
+		int textw2 = MeasureText(vidaText.c_str(), 40);
 
 		
 
@@ -62,14 +67,15 @@ int main()
 		BeginDrawing();
 		ClearBackground(WHITE);
 			
-		BeginMode2D(camera);
+		/*BeginMode2D(camera);*/
 		//cal ajustar mida per a que quadri ! ! ! ! ! ! ! ! ! !  ! !  ! ! ! 
 		if (game.bomberman.bmanPos.x > 555 && game.bomberman.bmanPos.x < 594) {
 			camera.target = { game.bomberman.bmanPos.x + 20, (float)screenHeight / 2 - 68};
 		}
 		DrawTexture(Fons, screenWidth / 2 - Fons.width / 2, screenHeight / 2 - Fons.height / 2, WHITE);
 		game.Draw();
-		DrawText(puntuacioText.c_str(), screenWidth - textWidth - 20, 20, 20, BLACK);
+		DrawText(puntuacioText.c_str(), screenWidth - textw1 - 50, 50, 50, BLACK);
+		DrawText(vidaText.c_str(), screenWidth - textw2 - 400, 230, 30, BLACK);
 		
 
 		EndMode2D();

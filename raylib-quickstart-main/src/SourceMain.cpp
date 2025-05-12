b2ad6aeb2558af9493c95e7d522fb2dbf1388133
guifre1714 <marcelpm2018@gmail.com>
2025-05-12 09:57:07 +0200

    orginitzacio menor

--- raylib-quickstart-main/src/SourceMain.cpp
+++ raylib-quickstart-main/src/SourceMain.cpp
@@ -33,12 +33,14 @@ int main()
 
 	Game game;
 
+	//interficie
 	int puntuacio = 0;
 	int vida = 3;
 	int contador = 200;
 	float temps = 0.0f;
 	const int fontSize = 65;
 
+	//Càmera
 	Camera2D camera = { 0 };
 	camera.target.x = 608;
 	camera.target.y = screenHeight/2.0f-20;
@@ -73,22 +75,17 @@ int main()
 		//interficie superior
 		float delta = GetFrameTime();
 		temps += delta;
-
 		if (temps >= 1.0f && contador > 0) 
 		{
 			contador--;
 			temps = 0.0f;
 		}
-
 		string puntuacioText = to_string(puntuacio);
 		int textw1 = MeasureText(puntuacioText.c_str(), 50);
-
 		string vidaText = "LEFT " + to_string(vida - 1);
 		int textw2 = MeasureText(vidaText.c_str(), 50);
-
 		const char* contadortext = TextFormat("%d", contador);
 		int textWidth = MeasureText(contadortext, fontSize);
-
 		string TIMEText = "TIME ";
 		int textw4 = MeasureText(TIMEText.c_str(), 50);
 
@@ -98,15 +95,16 @@ int main()
 		BeginDrawing();
 		ClearBackground(WHITE);
 
+		//INICI CÀMERA (estàtic respecte a la càmera)
 		BeginMode2D(camera);
-		//cal ajustar mida per a que quadri ! ! ! ! ! ! ! ! ! !  ! !  ! ! ! 
-		if (game.bomberman.bmanPos.x >= 588 && game.bomberman.bmanPos.x <= 652) {
+		if (game.bomberman.bmanPos.x >= 588 && game.bomberman.bmanPos.x <= 652) 
+		{
 			camera.target = { game.bomberman.bmanPos.x + 20, (float)screenHeight / 2 - 20};
 		}
 		DrawTexture(Fons, screenWidth / 2 - Fons.width / 2, screenHeight / 2 - Fons.height / 2, WHITE);
 		game.Draw();
 
-		
+		//FINAL CÀMERA (estàtic respecte a la pantalla)
 		EndMode2D();
 		DrawText(puntuacioText.c_str(), screenWidth - textw1 - 550, 32, 65, BLACK);
 		DrawText(vidaText.c_str(), screenWidth - textw2 - 110, 32, 65, BLACK);

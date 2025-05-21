a272f3c714243bc94b5b78f33d7765d994056c9e
mecMocmec <e.ibanez3426@gmail.com>
2025-05-21 12:55:59 +0200

    canvis

--- raylib-quickstart-main/src/endScreen.cpp
+++ raylib-quickstart-main/src/endScreen.cpp
@@ -10,13 +10,13 @@ endScreen::endScreen() {
 	Fons = LoadTexture("Sprites/menu/bombermanEndScreen.png");
 	bgm = LoadMusicStream("music/08. Ending.mp3");
 	
-	humanPos.x = 200;
-	humanPos.y = 100;
+	humanPos.x = 409;
+	humanPos.y = 272;
 
 	frameRec = { 0.0f, 0.0f, 16.0f, 16.0f };
 	currentFrame = 0;
 	frameContador = 0;
-	frameSpeed = 8; //marca la velocitat dels FPS
+	frameSpeed = 4; //marca la velocitat dels FPS
 	totalFrames = 3;
 	ampladaFrames = 16;
 }
@@ -26,6 +26,7 @@ endScreen:: ~endScreen() {
 }
 
 void endScreen::Draw() {
+	frameContador++;
 	if (frameContador >= (60 / frameSpeed)) {
 		frameContador = 0;
 		currentFrame++;
